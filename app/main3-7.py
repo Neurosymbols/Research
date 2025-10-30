66358ffe09aae18852e9ecc87741800208059a29
Mukund <mukund.shenoy@abakai.ai>
2025-10-30 19:34:13 +0530

    ontology improvements implemented 1. causal axioms for failure causes 2. rules in annotations 3. sign unicode fix 4. multi stage causes part of ontology

--- app/main3-7.py
+++ app/main3-7.py
@@ -134,10 +134,10 @@ def extract_specs():
     spec_category_dict = {}
     for row in target_cols:
         spec_name = row[1]
-        spec_category = row[2].split(":")[1].strip()
-        process_category = row[3].strip()
+        spec_category = row[3].split(":")[1].strip()
+        process_category = row[4].strip()
         spec_category_dict[normalize_text(spec_name.lower())] = {"onto_category": spec_category, "process_category": process_category}
-        spec_value = row[5]
+        spec_value = row[2]
         specs_dict[normalize_text(spec_name.lower())] = spec_value
     #chain the process
     # specs_dict = dict(list(specs_dict.items())[:3])
