bea5682cc3d008c33ced50470c250901cc75ddf7
Mukund <mukund.shenoy@abakai.ai>
2025-11-02 18:32:05 +0530

    test cases working

---
+++ app/services/metrics.py
@@ -0,0 +1,214 @@
+import pandas as pd
+import re
+import json
+from tabulate import tabulate
+from copy import copy
+import numpy as np
+
+from app.services.utils import perform_sparql_query
+
+def fc_name_syntax(classname):
+   # Split by any sequence of non-alphanumeric characters
+    parts = re.split(r'[\s]+', classname)
+    # Capitalize each part and join
+    if len(parts) > 1:
+        return "".join(word.capitalize() for word in parts if word)
+    elif len(parts) == 1:
+        return parts[0]
+
+def get_data_factory():
+    base_path = "./app/data"
+    path = f"{base_path}/input/epoch3-7/synthetic_data_factory.csv"
+    df = pd.read_csv(path)
+    factory = df.to_dict(orient="records")
+    return factory
+
+def get_chain_factory():
+    base_path = "./app/data"
+    path = f"{base_path}/input/epoch3-7/causal_test_cases.json"
+    factory = json.load(open(path))
+    new_factory = {}
+    for k, v in factory.items():
+        new_factory[k] = []
+        for chain in v:
+            new_chain = []
+            for chain_item in chain:
+                new_chain.append(fc_name_syntax(chain_item))
+            new_factory[k].append(new_chain)
+    return new_factory
+
+test_dict = {"Test Name": [], "System accuracy or response": []}
+
+#Metric 1
+def root_cause_accuracy():
+    factory = get_data_factory()
+    expected_set = {}
+    for d in factory:
+        if not pd.isna(d['Root Causes']):
+            root_fcs = [fc_name_syntax(fc) for fc in d['Root Causes'].split(",")]
+        else:
+            root_fcs = []
+        root_fcs = [fc for fc in root_fcs if fc]
+        if root_fcs:
+            expected_set[d['PCB_ID']] = root_fcs
+    
+    test_query = '''
+        PREFIX term: <https://neurosymbols.ai/ontology/causal-terminology.owl#>
+        PREFIX assert: <https://neurosymbols.ai/data/causal-assertions.owl#>
+        PREFIX iof: <https://spec.industrialontologies.org/ontology/core/Core/>
+        PREFIX bfo: <http://purl.obolibrary.org/obo/>
+        PREFIX prov: <http://www.w3.org/ns/prov#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX ro: <http://purl.obolibrary.org/obo/>
+
+        SELECT ?productlabel ?defectlabel ?rootcauselabel ?rule
+        WHERE {
+            ?defect a term:Defect ;
+                term:defectOccursOn ?product ;
+                ro:RO_0002559 ?rootcause ;
+                rdfs:label ?defectlabel .
+            ?rootcause a term:RootCause ;
+                    rdfs:label ?rootcauselabel ;
+    				prov:wasGeneratedBy ?ca .
+    		?ca term:triggeredByRule ?rule .
+            ?product rdfs:label ?productlabel .
+        }
+        ORDER BY ?product
+    '''
+    result_1 = perform_sparql_query(test_query)
+    result_1_bindings = result_1.get('results', {}).get('bindings', [])
+
+    pred_set = dict()
+    for b in result_1_bindings:
+        plabel = b.get("productlabel").get('value')
+        fclabel = b.get("rootcauselabel").get('value')
+        if plabel not in pred_set:
+            pred_set[plabel] = set()
+        pred_set[plabel].add(fclabel)
+    pred_set = {k:list(v) for k,v in pred_set.items()}
+
+    #top-k accuracy
+    total_boards_to_inspect = len(expected_set.keys())
+    top_k_test_passed_boards = []
+    equivalence_test_passed_boards = []
+    for k,v in pred_set.items():
+        #% of boards where at least one of the top-k predicted causes matches expert truth
+        intersection = list(set(v).intersection(set(expected_set[k])))
+        if len(intersection) > 0:
+            top_k_test_passed_boards.append(k)
+        #This test assesses strict semantic agreement between the system-inferred and expert-annotated root-cause sets.
+        if set(v) == set(expected_set[k]):
+            equivalence_test_passed_boards.append(k)
+    test_dict['Test Name'].extend(["top-k root cause", "root-cause set equivalence"])
+    test_dict['System accuracy or response'].extend([(len(top_k_test_passed_boards)/total_boards_to_inspect)*100, round((len(equivalence_test_passed_boards)/total_boards_to_inspect)*100,2)])
+
+
+#Metric 2
+def test_provenance_completeness():
+    test_query = '''
+        PREFIX base: <https://abakai.ai/ontology/semicon-base.owl#>
+        PREFIX term: <https://neurosymbols.ai/ontology/causal-terminology.owl#>
+        PREFIX assert: <https://neurosymbols.ai/data/causal-assertions.owl#>
+        PREFIX iof: <https://spec.industrialontologies.org/ontology/core/Core/>
+        PREFIX bfo: <http://purl.obolibrary.org/obo/>
+        PREFIX prov: <http://www.w3.org/ns/prov#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX ro: <http://purl.obolibrary.org/obo/>
+
+        SELECT (AVG(IF(BOUND(?gen) && BOUND(?der), 1, 0)) AS ?prov_completeness)
+        WHERE {
+        ?x a term:FailureCause .
+        ?x term:affects ?product .
+        ?x prov:wasGeneratedBy ?gen .
+        ?gen term:triggeredByRule ?der .
+        ?product a iof:MaterialProduct
+        }
+    '''
+    result_1 = perform_sparql_query(test_query)
+    result_1_bindings = result_1.get('results', {}).get('bindings', [])
+    for b in result_1_bindings:
+        prov_completeness = int(b.get("prov_completeness").get('value'))
+    test_dict['Test Name'].extend(["provenance completeness"])
+    test_dict['System accuracy or response'].extend([prov_completeness*100])
+
+#Metric 3
+def cycle_rate():
+    test_query = '''
+        PREFIX base: <https://abakai.ai/ontology/semicon-base.owl#>
+        PREFIX term: <https://neurosymbols.ai/ontology/causal-terminology.owl#>
+        PREFIX assert: <https://neurosymbols.ai/data/causal-assertions.owl#>
+        PREFIX iof: <https://spec.industrialontologies.org/ontology/core/Core/>
+        PREFIX bfo: <http://purl.obolibrary.org/obo/>
+        PREFIX prov: <http://www.w3.org/ns/prov#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX ro: <http://purl.obolibrary.org/obo/>
+
+        ASK WHERE {
+        ?x ro:causally_upstream_of+ ?x .
+        }
+        '''
+    result_1 = perform_sparql_query(test_query)
+    test_dict['Test Name'].extend(["cycle rate"])
+    if not result_1.get('boolean'):
+        test_dict['System accuracy or response'].extend(['No cycles detected in causal chains'])
+    else:
+       test_dict['System accuracy or response'].extend(['cycles detected in causal chains'])
+
+#Metric 4
+def chain_recall():
+    factory = get_chain_factory()
+    test_query = '''
+            PREFIX term: <https://neurosymbols.ai/ontology/causal-terminology.owl#>
+            PREFIX assert: <https://neurosymbols.ai/data/causal-assertions.owl#>
+            PREFIX iof: <https://spec.industrialontologies.org/ontology/core/Core/>
+            PREFIX bfo: <http://purl.obolibrary.org/obo/>
+            PREFIX prov: <http://www.w3.org/ns/prov#>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX ro: <http://purl.obolibrary.org/obo/>
+
+            SELECT ?productlabel ?effectlabel ?causelabel
+            WHERE {
+                ?effect a term:Effect .
+                ?cause a term:FailureCause .
+                VALUES ?effect_pred { term:defectOccursOn term:affects }
+                ?effect ?effect_pred ?product .
+                ?cause term:affects ?product .
+                ?effect term:directlyCausallyInfluencedBy ?cause .
+                ?product rdfs:label ?productlabel .
+                ?effect rdfs:label ?effectlabel .
+                ?cause rdfs:label ?causelabel
+                FILTER(?product = assert:{PCB})
+            }
+        '''
+    avg_recall = []
+    avg_precision = []
+    for k,v in factory.items():
+        query = test_query.replace("{PCB}", k)
+        result_1 = perform_sparql_query(query)
+        result_1_bindings = result_1.get('results', {}).get('bindings', [])
+        pred_set = []
+        for b in result_1_bindings:
+            # plabel = b.get("productlabel").get('value')
+            effectlabel = b.get("effectlabel").get('value')
+            causelabel = b.get("causelabel").get('value')
+            pred_set.append((effectlabel, causelabel))
+        pred_set = set((a.lower(), b.lower()) for a, b in pred_set)
+        v = set((a.lower(), b.lower()) for a, b in v)
+        intersection = pred_set & v
+        recall = round((len(intersection)/len(v))*100,2)
+        precision = round((len(intersection)/len(pred_set))*100,2)
+        avg_recall.append(recall)
+        avg_precision.append(precision)
+    test_dict['Test Name'].extend(["chain recall", "chain precision"])
+    test_dict['System accuracy or response'].extend([np.mean(avg_recall), np.mean(avg_precision)])
+
+root_cause_accuracy()
+test_provenance_completeness()
+cycle_rate()
+chain_recall()
+
+print(tabulate(test_dict, headers="keys", tablefmt="github"))
---
+++ app/tests/causal_chain_tests.py
@@ -0,0 +1,246 @@
+import pandas as pd
+import pytest
+
+from app.services.utils import perform_sparql_query, clear_graphdb_default_graph, export_ontology_to_graphdb
+from app.services.causal_chains import rule_to_sparql, fire_failure_cause_queries
+from app.services.utils import create_classname_syntax
+
+
+@pytest.fixture(scope="session")
+def data_factory():
+    base_path = "./app/data"
+    path = f"{base_path}/input/epoch3-7/synthetic_data_factory.csv"
+    df = pd.read_csv(path)
+    factory = df.to_dict(orient="records")
+    return factory
+
+def test_firing_of_failure_cause_rules_from_ishikawa_causal_graph():
+    conformance_count_query = '''
+        PREFIX term: <https://neurosymbols.ai/ontology/causal-terminology.owl#>
+        PREFIX assert: <https://neurosymbols.ai/data/causal-assertions.owl#>
+        PREFIX iof: <https://spec.industrialontologies.org/ontology/core/Core/>
+        PREFIX bfo: <http://purl.obolibrary.org/obo/>
+        PREFIX prov: <http://www.w3.org/ns/prov#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT (COUNT(?conformance) AS ?count)
+        WHERE {
+        ?conformance a term:ConformanceAssessment .
+        }
+    '''
+    conformance_with_required_edges_count = '''
+        PREFIX term: <https://neurosymbols.ai/ontology/causal-terminology.owl#>
+        PREFIX assert: <https://neurosymbols.ai/data/causal-assertions.owl#>
+        PREFIX iof: <https://spec.industrialontologies.org/ontology/core/Core/>
+        PREFIX bfo: <http://purl.obolibrary.org/obo/>
+        PREFIX prov: <http://www.w3.org/ns/prov#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT (COUNT(DISTINCT ?conformance) AS ?count)
+        WHERE {
+        ?conformance a term:ConformanceAssessment .
+        ?conformance prov:generated ?generated .
+        ?conformance term:hasAssessmentInput ?input .
+        ?conformace term:triggeredByRule ?rule .
+        }
+    '''
+    result_1 = perform_sparql_query(conformance_count_query)
+    result_2 = perform_sparql_query(conformance_with_required_edges_count)
+
+    result_1_bindings = result_1.get('results', {}).get('bindings', [])
+    result_2_bindings = result_2.get('results', {}).get('bindings', [])
+    assert len(result_1_bindings) > 0
+    assert len(result_2_bindings) > 0
+
+    assert result_1_bindings[0].get('count') == result_2_bindings[0].get('count')
+
+def test_predictability_of_firing_rules():
+    conformance_count_query = '''
+        PREFIX term: <https://neurosymbols.ai/ontology/causal-terminology.owl#>
+        PREFIX assert: <https://neurosymbols.ai/data/causal-assertions.owl#>
+        PREFIX iof: <https://spec.industrialontologies.org/ontology/core/Core/>
+        PREFIX bfo: <http://purl.obolibrary.org/obo/>
+        PREFIX prov: <http://www.w3.org/ns/prov#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT (COUNT(?conformance) AS ?count)
+        WHERE {
+        ?conformance a term:ConformanceAssessment .
+        }
+    '''
+    run1_result1 = perform_sparql_query(conformance_count_query)
+    run1_result1_bindings = run1_result1.get('results', {}).get('bindings', [])
+    run1_result1_count = run1_result1_bindings[0].get('count')
+
+    base_path = "./app/data"
+    path = f"{base_path}/ontologies/epoch3-7"
+    clear_graphdb_default_graph()
+    ontology_paths = [
+        f"{path}/bfo-prov.owl",
+        f"{path}/iof-core.rdf",
+        f"{path}/ro-causal-properties.owl",
+        f"{path}/causal-terminology.owl",
+        f"{path}/causal-assertions.owl"
+    ]
+    export_ontology_to_graphdb(
+        ontology_paths
+    )
+    rule_to_sparql(verb="INSERT")
+    fire_failure_cause_queries(verb="INSERT")
+
+    run2_result1 = perform_sparql_query(conformance_count_query)
+    run2_result1_bindings = run2_result1.get('results', {}).get('bindings', [])
+    run2_result1_count = run2_result1_bindings[0].get('count')
+
+    assert run1_result1_count == run2_result1_count
+
+def test_failure_cause_generation(data_factory):
+    factory = data_factory
+    expected_set = {}
+    for d in factory:
+        if not pd.isna(d['Mechanism Failure Causes']):
+            mech_fcs = [create_classname_syntax(fc) for fc in d['Mechanism Failure Causes'].split(",")]
+        else:
+            mech_fcs = []
+        if not pd.isna(d['Root Causes']):
+            root_fcs = [create_classname_syntax(fc) for fc in d['Root Causes'].split(",")]
+        else:
+            root_fcs = []
+        total_fcs = mech_fcs + root_fcs
+        total_fcs = [fc for fc in total_fcs if fc]
+        expected_set[d['PCB_ID']] = total_fcs
+    
+    test_query = '''
+        PREFIX term: <https://neurosymbols.ai/ontology/causal-terminology.owl#>
+        PREFIX assert: <https://neurosymbols.ai/data/causal-assertions.owl#>
+        PREFIX iof: <https://spec.industrialontologies.org/ontology/core/Core/>
+        PREFIX bfo: <http://purl.obolibrary.org/obo/>
+        PREFIX prov: <http://www.w3.org/ns/prov#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT ?productlabel ?failurecauselabel
+        WHERE {
+            ?failurecause a term:FailureCause ;
+                        term:affects ?product ;
+                        rdfs:label ?failurecauselabel ;
+                        prov:wasGeneratedBy ?coa .
+            ?product a iof:MaterialProduct ;
+                    rdfs:label ?productlabel .
+        }
+        ORDER BY ?product
+    '''
+    result_1 = perform_sparql_query(test_query)
+    result_1_bindings = result_1.get('results', {}).get('bindings', [])
+
+    pred_set = dict()
+    for b in result_1_bindings:
+        plabel = b.get("productlabel").get('value')
+        fclabel = b.get("failurecauselabel").get('value')
+        if plabel not in pred_set:
+            pred_set[plabel] = set()
+        pred_set[plabel].add(fclabel)
+    pred_set = {k:list(v) for k,v in pred_set.items()}
+
+    for k,v in pred_set.items():
+        assert set(expected_set[k]) == set(v)
+
+def test_root_cause_detection(data_factory):
+    factory = data_factory
+    expected_set = {}
+    for d in factory:
+        if not pd.isna(d['Root Causes']):
+            root_fcs = [create_classname_syntax(fc) for fc in d['Root Causes'].split(",")]
+        else:
+            root_fcs = []
+        root_fcs = [fc for fc in root_fcs if fc]
+        expected_set[d['PCB_ID']] = root_fcs
+
+    test_query = '''
+        PREFIX term: <https://neurosymbols.ai/ontology/causal-terminology.owl#>
+        PREFIX assert: <https://neurosymbols.ai/data/causal-assertions.owl#>
+        PREFIX iof: <https://spec.industrialontologies.org/ontology/core/Core/>
+        PREFIX bfo: <http://purl.obolibrary.org/obo/>
+        PREFIX prov: <http://www.w3.org/ns/prov#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX ro: <http://purl.obolibrary.org/obo/>
+
+        SELECT ?productlabel ?defectlabel ?rootcauselabel
+        WHERE {
+            ?defect a term:Defect ;
+                term:defectOccursOn ?product ;
+                ro:RO_0002559 ?rootcause ;
+                rdfs:label ?defectlabel .
+            ?rootcause a term:RootCause ;
+                    rdfs:label ?rootcauselabel .
+            ?product rdfs:label ?productlabel .
+        }
+        ORDER BY ?product
+    '''
+    result_1 = perform_sparql_query(test_query)
+    result_1_bindings = result_1.get('results', {}).get('bindings', [])
+
+    pred_set = dict()
+    for b in result_1_bindings:
+        plabel = b.get("productlabel").get('value')
+        fclabel = b.get("rootcauselabel").get('value')
+        if plabel not in pred_set:
+            pred_set[plabel] = set()
+        pred_set[plabel].add(fclabel)
+    pred_set = {k:list(v) for k,v in pred_set.items()}
+
+    for k,v in pred_set.items():
+        assert set(expected_set[k]) == set(v)
+
+def test_corrective_actions_assignment(data_factory):
+    factory = data_factory
+    expected_set = {}
+    for d in factory:
+        if not pd.isna(d['Corrective Actions']):
+            cas = [ca.lower().strip() for ca in d['Corrective Actions'].split(",")]
+        else:
+            cas = []
+        expected_set[d['PCB_ID']] = cas
+
+    test_query = '''
+        PREFIX term: <https://neurosymbols.ai/ontology/causal-terminology.owl#>
+        PREFIX assert: <https://neurosymbols.ai/data/causal-assertions.owl#>
+        PREFIX iof: <https://spec.industrialontologies.org/ontology/core/Core/>
+        PREFIX bfo: <http://purl.obolibrary.org/obo/>
+        PREFIX prov: <http://www.w3.org/ns/prov#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX ro: <http://purl.obolibrary.org/obo/>
+
+        SELECT ?productlabel ?defectlabel ?rootcauselabel ?calabel
+        WHERE {
+            ?defect a term:Defect ;
+                term:defectOccursOn ?product ;
+                ro:RO_0002559 ?rootcause ;
+                rdfs:label ?defectlabel .
+            ?rootcause a term:RootCause ;
+                    rdfs:label ?rootcauselabel ;
+                    term:isCorrectedBy ?ca .
+            ?product rdfs:label ?productlabel .
+            ?ca rdfs:label ?calabel
+        }
+        ORDER BY ?product
+    '''
+    result_1 = perform_sparql_query(test_query)
+    result_1_bindings = result_1.get('results', {}).get('bindings', [])
+
+    pred_set = dict()
+    for b in result_1_bindings:
+        plabel = b.get("productlabel").get('value')
+        calabel = b.get("calabel").get('value').lower()
+        if plabel not in pred_set:
+            pred_set[plabel] = set()
+        pred_set[plabel].add(calabel)
+    pred_set = {k:list(v) for k,v in pred_set.items()}
+
+    for k,v in pred_set.items():
+        assert set(expected_set[k]) == set(v)
